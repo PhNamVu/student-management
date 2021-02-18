12b83390daa609b63ab24a231d24f3721bc40ed4
Nam Vu <phnamvu@gmail.com>
2021-02-18 20:17:50 +0700

    6hzwn5-6hzwu4/improve-nav-bar-and-log-out-function

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Container } from 'reactstrap'
 import { LoginPage } from './Pages/LoginPage'
 import { CreateMagazine } from './Pages/CreateMagazine'
 import { ToasterContainer } from 'baseui/toast'
-import { NavBar } from './components/AdminNav'
+import { AdminNav } from './components/AdminNav'
 
 import { useAuth } from './hooks/use-auth'
 import createUnAuthClient from './apollo/unauth-client'
@@ -76,7 +76,7 @@ function App() {
           <BrowserRouter>
             <ToasterContainer>
               <ApolloProvider client={client}>
-                <NavBar></NavBar>
+                <AdminNav/>
                 <Container
                   fluid
                   style={{ backgroundColor: '#FFFFFF', minHeight: '100vh' }}
